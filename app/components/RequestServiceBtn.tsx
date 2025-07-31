c6d1e357a7d72f1895971600a9b0f55fdffbbb8c
Cláudia Vieira <claudiasofia4@gmail.com>
2025-07-31 11:53:39 +0100

    Create header

---
+++ app/components/RequestServiceBtn.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from "react";
+
+const RequestService = () => {
+    return (
+        <a 
+            className="text-primary border-1 border-primary rounded-lg px-10 content-center h-14"
+            href="https://wa.link/1v15kn"
+            target="_blank"
+        >
+            Pedir Arranjo
+        </a>
+    )
+}
+
+export default RequestService;
