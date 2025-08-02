e3199c7075dd2913060643b3342e12cc463a1b46
Cláudia Vieira <claudiasofia4@gmail.com>
2025-08-02 20:22:07 +0100

    Keep work on text cards

--- app/components/RequestServiceBtn.tsx
+++ app/components/RequestServiceBtn.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function RequestService() {
     return (
         <a 
-            className="text-primary border-1 border-primary rounded-lg px-10 content-center h-14"
+            className="text-primary border-1 border-primary rounded-lg px-6 content-center h-10 uppercase text-[14px]"
             href="https://wa.link/1v15kn"
             target="_blank"
         >
--- app/page.tsx
+++ app/page.tsx
@@ -5,46 +5,55 @@ import { BsExclamationCircle, BsCheck2Circle } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <>
-      <header className="flex bg-primary justify-between items-center pl-6 pr-6">
-        <Image
-            className="items-left"
-            src="/logo_deitado_azul_com-fundo.jpg"
-            alt="Costura À Porta logo"
-            width={180}
-            height={18}
-            priority
-          />
-          <RequestService />
+    <div>
+      <header>
+        <div className="container mx-auto px-6 lg:px-8">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <Image
+                  className="items-left h-16 w-auto"
+                  src="/logo_deitado_azul_sem-fundo.png"
+                  alt="Costura À Porta logo"
+                  width={180}
+                  height={18}
+                  priority
+                />
+            </div>
+              <RequestService />
+          </div>
+        </div>
       </header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-background">
-        <section>
+      <main className="mx-auto">
+        <section className="bg-primary">
           HERO
         </section>
-        <section className="bg-primary text-background">
-          <div>
-            <ul className="flex items-center justify-between">
-              <li className="m-[20px] pl-[20px] border-l-4 border-l-indigo-500 shadow-2xs">
-                <div className="flex items-center">
-                  <BsExclamationCircle />
-                  <h2 className="ml-2 text-lg font-bold">O PROBLEMA</h2>
-                </div>
-                <p>Com rotinas apertadas, encontrar tempo para ajustes de roupa é difícil devido a deslocações e falta de serviços flexíveis e de qualidade.</p>
-              </li>
-              <li className="m-[20px] pl-[20px] border-l-4 border-l-indigo-500 shadow-2xs ">
-                <div className="flex items-center">
-                  <BsCheck2Circle />
-                  <h2 className="ml-2 text-lg font-bold">A SOLUÇÃO</h2>
-                </div>
-                <p>Serviço de costura ao domicílio que recolhe, ajusta e entrega as suas peças com rapidez e atendimento personalizado, tudo gerido por uma plataforma digital intuitiva.</p>
-              </li>
-            </ul>
-          </div>
+        <section className="bg-background text-primary">
+          <ul>
+            <li className="flex items-baseline gap-2 m-[20px] pl-[20px] p-[30px] border-l-4 border-l-indigo-500 shadow-lg rounded-md">
+              <div className="bg--gray rounded-full flex-shrink-0">
+                <BsExclamationCircle size={25} strokeWidth={0.25} />
+              </div>
+              <div>
+              <h2 className="ml-2 mb-[20px] text-2xl font-bold">O PROBLEMA</h2>
+              <p>Com rotinas apertadas, encontrar tempo para ajustes de roupa é difícil devido a deslocações e falta de serviços flexíveis e de qualidade.</p>
+              </div>
+              
+            </li>
+            <li className="flex items-baseline gap-2 m-[20px] pl-[20px] p-[30px] border-l-4 border-l-indigo-500 shadow-lg rounded-md">
+              <div>
+                <BsCheck2Circle size={25} strokeWidth={0.25} />
+              </div>
+              <div>
+              <h2 className="ml-2 mb-[20px] text-2xl font-bold">A SOLUÇÃO</h2>
+              <p>Serviço de costura ao domicílio que recolhe, ajusta e entrega as suas peças com rapidez e atendimento personalizado, tudo gerido por uma plataforma digital intuitiva.</p>
+              </div>
+            </li>
+          </ul>
         </section>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         
       </footer>
-    </>
+    </div>
   );
 }
