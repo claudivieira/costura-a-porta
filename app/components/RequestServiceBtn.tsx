5941c2854f7b2404309a2e8c0b0f565f2d75a438
Cláudia Vieira <claudiasofia4@gmail.com>
2025-08-14 16:08:24 +0100

    Add analytics and make some cleanuo

--- app/components/RequestServiceBtn.tsx
+++ app/components/RequestServiceBtn.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { sendGTMEvent } from '@next/third-parties/google'
+
 
 type requestServiceBtnProps = {
-  extraStyles: string
+  extraStyles: string,
+  placement?: string
 }
 
 
-const RequestServiceBtn = ({ extraStyles }: requestServiceBtnProps) => {
+const RequestServiceBtn = ({ extraStyles, placement }: requestServiceBtnProps) => {
   return (
     <a 
             className={`border rounded-lg px-6 h-10 uppercase inline-flex items-center justify-center ${extraStyles}`}
             href="https://wa.link/1v15kn"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={() => sendGTMEvent({ event: 'buttonClicked', value: `Request Service ${placement}` })}
         >
             Pedir Arranjo
         </a>
