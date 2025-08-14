8ba661946778a4c7d10bbf0b5386e542d32c48ad
Cláudia Vieira <claudiasofia4@gmail.com>
2025-08-14 17:16:27 +0100

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -21,6 +22,7 @@ export default function RootLayout({
         className={montserrat.className}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
